78822832e54c0af3da99b6313c44cb7c62fa186c
Kat Maldon <katharinamaldon@gmail.com>
2019-06-27 18:50:51 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  attr_reader :name, :weight, :height
+
+  PIRATES = []
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
+
 end
